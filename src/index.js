8d37717a48e6fd58d16a2be714e380a46885b07e
isys35 <isys35@mail.ru>
2021-08-23 16:52:40 +0300

    chapter 5

--- src/index.js
+++ src/index.js
@@ -2,39 +2,39 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />,
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
+
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Привет, мир!</h1>
+                <h2>Сейчас {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    )
-}
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+)
 
-
-function UserInfo(props) {
-    return (
-        <Avatar user={props.user} />,
-        <div className="UserInfo-name">
-            {props.user.name}
-        </div>
-    )
-}
-
-/*const element = <Welcome name="ДИМОООООН"/>*/
-
-ReactDOM.render(<App />, document.getElementById("root"))
