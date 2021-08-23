e2b7837b7d5c091397c96245f83a821c42e4e751
isys35 <isys35@mail.ru>
2021-08-23 17:12:27 +0300

    chapter 6.3

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import './index.css';
 class LoggingButton extends React.Component {
   // Такой синтаксис гарантирует, что `this` привязан к handleClick.
     // Предупреждение: это экспериментальный синтаксис
-  handleClick = () => {  console.log('значение this:', this);  }
+  handleClick() {
+    console.log('значение this:', this);
+  }
   render() {
     return (
-      <button onClick={this.handleClick}>
+      <button onClick={() => this.handleClick()}>
         Нажми на меня
       </button>
     );
