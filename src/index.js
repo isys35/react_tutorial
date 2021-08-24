75b298774a618de4944ab39c81499d5c36c9586e
isys35 <isys35@mail.ru>
2021-08-24 17:02:04 +0300

    chapter 7.1

--- src/index.js
+++ src/index.js
@@ -4,23 +4,37 @@ import './index.css';
 
 
 
-class LoggingButton extends React.Component {
-  handleClick() {
-    console.log('значение this:', this);
-  }
-  render() {
-    return (
-      <button onClick={() => this.handleClick()}>
-        Нажми на меня
-      </button>
-    );
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+      }
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+      }
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />
+        }
+       return (
+         <div>
+             <Greeting isLoggedIn={isLoggedIn} />
+             {button}
+         </div>
+       );
+      }
   }
 }
 
-<button onClick={(e) => this.deleteRow(id, e)}>Удалить строку</button>
-<button onClick={this.deleteRow(this, id)}>Удалить строку</button>
-
 ReactDOM.render(
-    <LoggingButton />,
+    <LoginControl />,
     document.getElementById('root')
 )
