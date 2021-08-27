9bdf6ee65a7097d1c29b02fb1db455ce91774785
isys35 <isys35@mail.ru>
2021-08-27 15:23:05 +0300

    chapter 7

--- src/index.js
+++ src/index.js
@@ -2,89 +2,41 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
-function UserGreeting(props) {
-  return <h1>С возвращением!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Войдите, пожалуйста.</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
+function WarningBanner(props) {
+  if (!props.warn){
+    return null;
   }
-  return <GuestGreeting />
-}
-
-function LoginButton(props) {
   return (
-      <button onClick={props.onClick}>
-        Войти
-      </button>
-  )
-}
-function LogoutButton(props) {
-  return (
-      <button onClick={props.onClick}>
-        Выйти
-      </button>
-  )
-}
-
-
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
+      <div className="warning">
+        Предупреждение!
       </div>
-    );
-  }
-}
-
-function Mailbox(props) {
-  const unreadMessages = props.unreadMessages;
-  return (
-    <div>
-      <h1>Здравствуйте!</h1>
-      {unreadMessages.length > 0 &&
-      <h2>
-        У вас {unreadMessages.length} непрочитанных сообщений.
-      </h2>
-      }
-    </div>
   );
 }
 
-const messages = ['React', 'Re: React', 'Re:Re: React', 'Re:Re: React'];
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+  render() {
+    return (
+        <div>
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handleToggleClick}>
+            {this.state.showWarning ? 'Спрятать' : 'Показать'}
+          </button>
+        </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Mailbox unreadMessages={messages} />,
+  <Page />,
   document.getElementById('root')
 );
