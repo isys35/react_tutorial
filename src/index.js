127934277876bd9fb77113d70528aabb2ee04b63
isys35 <isys35@mail.ru>
2021-09-03 16:12:54 +0300

    chapter 9.3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class EssayForm extends React.Component {
+class FlavorForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 'Будьте любезны, напишите сочинение о вашем любимом DOM-элементе.'
+            value: 'coconut'
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,15 +18,21 @@ class EssayForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('Сочинение отправлено: ' + this.state.value);
+        alert('Ваш любимый вкус: ' + this.state.value);
         event.preventDefault();
     }
     render() {
         return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Сочинение:
-          <textarea value={this.state.value} onChange={this.handleChange} />        </label>
+          Выберите ваш любимый вкус:
+            <select value={this.state.value} onChange={this.handleChange}>
+                <option value="grapefruit">Грейпфрут</option>
+                <option value="lime">Лайм</option>
+                <option value="coconut">Кокос</option>
+                <option value="mango">Кокос</option>
+            </select>
+        </label>
         <input type="submit" value="Отправить" />
       </form>
     );
@@ -35,6 +41,6 @@ class EssayForm extends React.Component {
 
 
 ReactDOM.render(
-  <EssayForm />,
+  <FlavorForm />,
   document.getElementById('root')
 );
