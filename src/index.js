4cf403b200db78501200483daf9f8ce96295f7fb
isys35 <isys35@mail.ru>
2021-09-03 16:07:35 +0300

    chapter 9.2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class NameForm extends React.Component {
+class EssayForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
-
+        this.state = {
+            value: 'Будьте любезны, напишите сочинение о вашем любимом DOM-элементе.'
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -17,23 +18,23 @@ class NameForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('Отправленное имя: ' + this.state.value);
+        alert('Сочинение отправлено: ' + this.state.value);
         event.preventDefault();
     }
-
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Имя:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Отправить"/>
-            </form>
-        )
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Сочинение:
+          <textarea value={this.state.value} onChange={this.handleChange} />        </label>
+        <input type="submit" value="Отправить" />
+      </form>
+    );
     }
 }
+
+
 ReactDOM.render(
-  <NameForm />,
+  <EssayForm />,
   document.getElementById('root')
 );
