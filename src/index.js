8bcbd1d16eb32bed2e67f91be3399c32aa831ac6
isys35 <isys35@mail.ru>
2021-09-07 16:39:11 +0300

    chapter 10.1

--- src/index.js
+++ src/index.js
@@ -3,49 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Reservation extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isGoing: true,
-            numberOfGuests: 2
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>Вода закипит.</p>
     }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    render() {
-        return (
-            <form>
-                <label>
-                    Пойдут:
-                    <input name='isGoing'
-                           type="checkbox"
-                           checked={this.state.isGoing}
-                           onChange={this.handleInputChange} />
-                </label>
-                <br/>
-                <label>
-                    <input  name='numberOfGuests' 
-                            type="number"
-                            value={this.state.numberOfGuests}
-                            onChange={this.handleInputChange}/>
-                </label>
-            </form>
-        )
-    }
+    return <p>Вода не закипит.</p>
 }
 
 ReactDOM.render(
-  <Reservation />,
+  <BoilingVerdict celsius={55}/>,
   document.getElementById('root')
 );
