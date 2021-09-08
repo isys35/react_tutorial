58fdcee13c3e6d6dd91132709a73c8a2a130f2c4
isys35 <isys35@mail.ru>
2021-09-08 16:45:12 +0300

    chapter 10.3

--- src/index.js
+++ src/index.js
@@ -10,29 +10,67 @@ function BoilingVerdict(props) {
     return <p>Вода не закипит.</p>
 }
 
-class Calculator extends React.Component {
+const scaleNames = {
+    c: 'Цельсия',
+    f: 'Фаренгейта'
+}
+
+
+class TemperatureInput extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
         this.state = {temperature: ''}
     }
-    
+
     handleChange(e) {
-        this.setState({temperature: e.target.value})
+        this.setState({temperature: e.target.value});
     }
-    
+
     render() {
         const temperature = this.state.temperature;
+        const scale = this.props.scale;
         return (
             <fieldset>
-                <legend>Введите температуру в градусах Цельсия:</legend>
+                <legend>Введите температуру в градусах {scaleNames[scale]}:</legend>
                 <input value={temperature} onChange={this.handleChange}/>
-                <BoilingVerdict 
-                    celsius={parseFloat(temperature)} />
             </fieldset>
         )
     }
+
 }
+
+class Calculator extends React.Component {
+    render() {
+        return (
+            <div>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </div>
+        )
+    }
+}
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/ 9;
+}
+
+
+function toFarenheit(celsius) {
+    return (celsius * 9 / 5) +32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+
 
 ReactDOM.render(
   <Calculator />,
