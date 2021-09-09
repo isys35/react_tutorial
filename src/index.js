b274a8d3fc86bdef17079b9fbef556b8feb0e90c
isys35 <isys35@mail.ru>
2021-09-09 16:52:45 +0300

    chapter 11.3

--- src/index.js
+++ src/index.js
@@ -24,8 +24,40 @@ function WelcomeDialog() {
     );
 }
 
+function SplitPane(props) {
+    return (
+        <div className="SplitPane">
+            <div className="SplitPane-left">
+                {props.left}
+            </div>
+            <div className="SplitPane-right">
+                {props.right}
+            </div>
+        </div>
+    )
+}
+
+function Contacts() {
+    return (
+        <p>Контакты</p>
+    )
+}
+
+
+function Chat() {
+    return (
+        <p>Чат</p>
+    )
+}
+
+function App() {
+    return (
+        <SplitPane left={<Contacts />} right={<Chat />}></SplitPane>
+    );
+}
+
 
 ReactDOM.render(
-  <WelcomeDialog />,
+  <App />,
   document.getElementById('root')
 );
